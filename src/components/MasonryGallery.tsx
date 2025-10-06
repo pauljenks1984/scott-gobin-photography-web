3eec059a52279102605069d47589ef066cebe949
Paul Jenkinson <pjenkinsonmail@gmail.com>
2025-10-06 16:45:18 +0100

    responsive fix 2

--- src/components/MasonryGallery.tsx
+++ src/components/MasonryGallery.tsx
@@ -1,6 +1,5 @@
 // src/components/MasonryGallery.tsx
 import Masonry from "react-masonry-css";
-import ImageCard from "./ImageCard";
 
 type ImageResource = {
   public_id: string;
@@ -8,19 +7,39 @@ type ImageResource = {
   metadata?: Record<string, any>;
 };
 
-export default function MasonryGallery({ images }: { images: ImageResource[] }) {
+export default function MasonryGallery({
+  images,
+  onClick,
+}: {
+  images: ImageResource[];
+  onClick?: (index: number) => void;
+}) {
   return (
     <Masonry
       breakpointCols={{
-        default: 3,   // desktop (>= 1024px)
-        1024: 2,      // tablet
-        640: 1        // mobile
+        default: 3,
+        1280: 3,
+        1024: 2,
+        768: 1,
+        640: 1,
       }}
       className="flex w-full gap-4"
       columnClassName="flex flex-col gap-4"
     >
-      {images.map((img) => (
-        <ImageCard key={img.public_id} image={img} />
+      {images.map((img, index) => (
+        <div
+          key={img.public_id}
+          className="relative w-full cursor-pointer overflow-hidden rounded"
+          style={{ breakInside: "avoid" }}
+          onClick={() => onClick?.(index)}
+        >
+          <img
+            src={img.secure_url}
+            alt={img.public_id}
+            loading="lazy"
+            className="w-full h-auto object-cover"
+          />
+        </div>
       ))}
     </Masonry>
   );
